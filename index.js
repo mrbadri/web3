d3829ec8616905052f204e52a737f4dfa3be89ea
MrBadri <badri0000badri@gmail.com>
2023-05-05 14:24:36 +0430

    [Feat]: handle web3 function with ganache

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const Web3 = require("web3");
 const URL = "https://mainnet.infura.io/v3/a9b1826d4e1441ae8c08d2fdebff62e0";
-const address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const ganacheURL = "http://127.0.0.1:7545";
+const builtInURL = Web3.givenProvider || "ws://localhost:8545";
 
-const web3 = new Web3(URL);
+const address = "0xdC0f4EB5Cbb62ae7927bD3EA0f8BFc31fAb9094B";
 
-let balance = "0";
+const web3 = new Web3(ganacheURL);
 
-web3.eth.getBalance(address).then((res) => {
-  balance = res;
-  console.log(web3.utils.fromWei(balance, "ether"));
-});
+const getBalance = (address) => {
+  web3.eth.getBalance(address).then((res) => {
+    console.log(web3.utils.fromWei(res, "ether"));
+  });
+};
 
-// web3.utils.fromWei(, "ether");
+const newAccount = web3.eth.accounts.create();
 
-// console.log(web3(URL));
-// web3.eth.getBalance(address, (error, balance) => {
-//   console.log(balance);
-// });
+console.log(newAccount);
+
+getBalance(address);
