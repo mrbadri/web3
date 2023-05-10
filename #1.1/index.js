a6d6c9622961ff191e28d918a76e4521171aeaf7
MrBadri <badri0000badri@gmail.com>
2023-05-11 01:35:38 +0430

    clean code

--- #1.1/index.js
+++ #1.1/index.js
@@ -1,20 +1,19 @@
-const Web3 = require('web3');
-const web3 = new Web3('http://127.0.0.1:7545'); 
+const Web3 = require("web3");
+const web3 = new Web3("http://127.0.0.1:7545");
 
-const privateKey = '0xd068f7095d361bae8bb821fb09bcf0be4fc2a788e6fe96c2867066ab7f122566'; 
-const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+const fromAddress = "0xd05b804FF3A513645ce1faB5909546C9E72e0eD8";
+const toAddress = "0x3B7ca7e8e3fF62391fb9F6E1f4b60665E6ab7922";
+const value = web3.utils.toWei("1", "ether");
 
-const toAddress = '0x3B7ca7e8e3fF62391fb9F6E1f4b60665E6ab7922'; 
-const value = web3.utils.toWei('1', 'ether'); 
-
-web3.eth.sendTransaction({
-  from: account.address,
-  to: toAddress,
-  value: value
-})
-.then((receipt) => {
-  console.log('Transaction receipt:', receipt);
-})
-.catch((error) => {
-  console.error('Error sending transaction:', error);
-});
+web3.eth
+  .sendTransaction({
+    from: fromAddress,
+    to: toAddress,
+    value: value,
+  })
+  .then((receipt) => {
+    console.log("Transaction receipt:", receipt);
+  })
+  .catch((error) => {
+    console.error("Error sending transaction:", error);
+  });
